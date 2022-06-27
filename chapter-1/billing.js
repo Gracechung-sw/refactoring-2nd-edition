dca48940e307562a7f272dcf1faa4979a6c5c9ae
Gracechung-sw <hjngy0511@gmail.com>
2022-06-28 01:35:34 +0900

    함수 쪼개기

--- chapter-1/billing.js
+++ chapter-1/billing.js
@@ -10,27 +10,8 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
 
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 20);
-        }
-        break;
-
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -39,10 +20,34 @@ function statement(invoice, plays) {
     result += `${play.name}: ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
-    totalAmount += this.thisAmount;
+    totalAmount += thisAmount;
   }
 
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점 \n`;
+  return result;
+}
+
+function amountFor(aPerformance, play) {
+  let result = 0;
+  switch (play.type) {
+    case 'tragedy':
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 20);
+      }
+      break;
+
+    case 'comedy':
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르 : ${play.type}`);
+  }
   return result;
 }
