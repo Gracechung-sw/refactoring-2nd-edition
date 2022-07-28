6f19157a6eff69e35174b684fe8d8985683f0d2c
Gracechung-sw <hjngy0511@gmail.com>
2022-07-28 09:39:18 +0900

    Refactor: Rename variable

--- ch6/6-7.js
+++ ch6/6-7.js
@@ -1,6 +1,6 @@
-let a = height * width;
+let area = height * width;
 
-const cpyNm = '애플';
+const companyName = '애플';
 
-let tpHd = '제목없음';
-let result = `<h1>${tpHd}</h1>`;
+let title = '제목없음';
+let result = `<h1>${title}</h1>`;
