f7971e6556cf849a8831ddb6e135bb78e29ae4e0
Gracechung-sw <hjngy0511@gmail.com>
2022-07-28 09:25:37 +0900

    Refactor: Extract Variable

--- ch6/6-3-1.js
+++ ch6/6-3-1.js
@@ -1,8 +1,7 @@
 export function price(order) {
-  // 가격(price) = 기본가격 - 수량할인 + 배송비
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  const basePrice = order.quantity * order.itemPrice;
+  const discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+
+  return basePrice - discount + shipping;
 }
