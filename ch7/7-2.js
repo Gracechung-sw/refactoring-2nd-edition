86c0a0f9603225286822e201a60f2b3e44f6a281
Gracechung-sw <hjngy0511@gmail.com>
2022-09-02 09:29:49 +0900

    Refactor: Collection Capsulation

--- ch7/7-2.js
+++ ch7/7-2.js
@@ -11,11 +11,20 @@ export class Person {
   }
 
   get courses() {
-    return this.#courses;
+    return [...this.#courses]; // 새로운 배열을 만들어서 전달. 그래서 외부에서 이렇게 전달받은 데이터를 수정하더라도 기존 데이터에는 영향을 미치지 않음.
   }
 
-  set courses(courses) {
-    this.#courses = courses;
+  addCourse(course) {
+    this.#courses.push(course);
+  }
+
+  removeCourse(course, runIfAbsent) {
+    const index = this.#courses.indexOf(course);
+    if (index == -1) {
+      runIfAbsent();
+      return;
+    }
+    this.#courses.splice(index, 1);
   }
 }
 
@@ -37,5 +46,16 @@ export class Course {
 }
 
 const ellie = new Person('엘리');
-ellie.courses.push(new Course('리팩토링', true));
+/**
+ * Refactor point: 읽어온 courses data를 외부에서 마음대로 update, delete할 수 있음.
+ *
+ * 그래서 data에 대해 할 수 있는 동작만 외부에 공개하는 캡슐화가 필요.
+ */
+// ellie.courses.push(new Course('리팩토링', true));
+const course = new Course('리팩토링', true);
+ellie.addCourse(course);
+
+ellie.removeCourse(course, () => {
+  console.log('no course');
+});
 console.log(ellie.courses.length);
