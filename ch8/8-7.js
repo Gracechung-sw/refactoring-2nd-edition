470593a21c510ffe4ea8137955175e3d2ea373ad
Gracechung-sw <hjngy0511@gmail.com>
2022-09-22 09:48:38 +0900

    Refactor: more simple code using api

--- ch8/8-7.js
+++ ch8/8-7.js
@@ -12,18 +12,10 @@ export function reportYoungestAgeAndTotalSalary(people) {
   return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
 
   function youngestAge() {
-    let youngest = people[0] ? people[0].age : Infinity;
-    for (const p of people) {
-      if (p.age < youngest) youngest = p.age;
-    }
-    return youngest;
+    return Math.min(...people.map((p) => p.age));
   }
 
   function totalSalary() {
-    let totalSalary = 0;
-    for (const p of people) {
-      totalSalary += p.salary;
-    }
-    return totalSalary;
+    return people.reduce((total, p) => (total += p.salary), 0);
   }
 }
