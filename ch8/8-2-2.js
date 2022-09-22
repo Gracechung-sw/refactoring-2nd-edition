a31bced7d37a913dc4f9528cf09691520d00f2aa
Gracechung-sw <hjngy0511@gmail.com>
2022-09-22 09:00:45 +0900

    Refactor: Add Refactor point of move field and refactor

--- ch8/8-2-2.js
+++ ch8/8-2-2.js
@@ -1,17 +1,21 @@
 class Account {
-  constructor(number, type, interestRate) {
+  constructor(number, type) {
     this._number = number;
     this._type = type;
-    this._interestRate = interestRate;
   }
 
   get interestRate() {
-    return this._interestRate;
+    return this._type._interestRate;
   }
 }
 
 class AccountType {
   constructor(nameString) {
     this._name = nameString;
+    this._interestRate = interestRate;
+  }
+
+  get interestRate() {
+    return this._interestRate;
   }
 }
