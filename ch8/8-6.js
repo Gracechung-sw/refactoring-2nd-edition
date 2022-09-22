f1458cd32c484c7f995d6f865a3fb6b37bcc4f5d
Gracechung-sw <hjngy0511@gmail.com>
2022-09-22 09:35:27 +0900

    Refactor: Add Refactor point of Slide Statements and refactor

--- ch8/8-6.js
+++ ch8/8-6.js
@@ -1,3 +1,9 @@
+/**
+ * 문장 슬라이스하기(Slide Statements)
+ * Refactoring point
+ *  중복 코드 없애기
+ *  그리고 연관있는 코드끼리 모아두기
+ */
 // 예제 1
 const pricingPlan = retrievePricingPlan();
 const order = retreiveOrder();
@@ -6,13 +12,10 @@ const chargePerUnit = pricingPlan.unit;
 
 // 예제 2
 function someFunc() {
-  let result;
-  if (availableResources.length === 0) {
-    result = createResource();
-    allocatedResources.push(result);
-  } else {
-    result = availableResources.pop();
-    allocatedResources.push(result);
-  }
+  const result =
+    availableResources.length === 0
+      ? createResource()
+      : availableResources.pop();
+  allocatedResources.push(result);
   return result;
 }
