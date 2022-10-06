9b07795cd1310565c31278ac49060a8df0ac5f6c
Gracechung-sw <hjngy0511@gmail.com>
2022-10-06 10:46:01 +0900

    Refactor: Replace Nested Conditional with Guard Clauses

--- ch10/10-3-2.js
+++ ch10/10-3-2.js
@@ -1,11 +1,16 @@
 export function adjustedCapital(instrument) {
-  let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+  if (!isEligibleForAdjustedCapital(instrument)) {
+    return 0;
   }
-  return result;
+  return (
+    (instrument.income / instrument.duration) * anInstrument.adjustmentFactor
+  );
+}
+
+function isEligibleForAdjustedCapital(instrument) {
+  return (
+    instrument.capital > 0 &&
+    instrument.interestRate > 0 &&
+    instrument.duration > 0
+  );
 }
