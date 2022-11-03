34d77370b18653e95b3ad7512ffc970b7ed7ca81
Gracechung-sw <hjngy0511@gmail.com>
2022-11-03 21:21:03 +0900

    Refactor: Return Modified Value

--- ch11/11-11.js
+++ ch11/11-11.js
@@ -1,13 +1,17 @@
-export function ascentVelocity(points, totalMinutes) {
-  function calculateAscent() {
+/**
+ * 수정된 값 반환하기
+ */
+export function ascentVelocity(totalMinutes) {
+  function calculateAscent(points) {
+    let result = 0;
     for (let i = 1; i < points.length; i++) {
       const verticalChange = points[i].elevation - points[i - 1].elevation;
-      totalAscent += verticalChange > 0 ? verticalChange : 0;
+      result += verticalChange > 0 ? verticalChange : 0;
     }
+    return result
   }
 
-  let totalAscent = 0;
-  calculateAscent([{ elevation: 10 }, { elevation: 20 }]);
+  let totalAscent = calculateAscent([{ elevation: 10 }, { elevation: 20 }]); // calculateAscent 함수 내부에서 외부 변수인 totalAscent의 값을 직접 변경하고 있다. -> 냄새나는 코드.
 
   return totalAscent / totalMinutes;
 }
