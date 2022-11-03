c0971bf52b8bd4d2ef21956ebd8760979dca884b
Gracechung-sw <hjngy0511@gmail.com>
2022-11-03 21:19:39 +0900

    Refactor: Replace Query with Parameter

--- ch11/11-6.js
+++ ch11/11-6.js
@@ -1,7 +1,19 @@
+/**
+ * 
+ */
 targetTemperature(aPlan);
 
 // 다른모듈에 있는 함수라고 가정
 function targetTemperature(aPlan) {
-  currentTemperature = thermostat.currentTemperature;
+  currentTemperature = thermostat.currentTemperature; // 어떤 특정 객체에 접근하고 있는데, 이 객체가 
+  // 전역 변수이거나 또는 다른 모듈에 있다면, 
+  // 불변성에도 안좋고, 다른 모듈과 심각하게 coupling되어 있다는 말이다. 
+  // 그래서 이런건 함수 매개변수로 외부에서 전달 받는 것이 좋다. 
   // ...
+}
+
+
+function targetTemperature(aPlan, currentTemperature) {
+  currentTemperature = currentTemperature;
+    // ...
 }
