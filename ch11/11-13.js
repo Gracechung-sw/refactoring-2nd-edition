3e576e19e683e5d9cb5e65a3a94e6713b534afc8
Gracechung-sw <hjngy0511@gmail.com>
2022-11-03 21:21:43 +0900

    Refactor: Replace Exception with Precheck

--- ch11/11-13.js
+++ ch11/11-13.js
@@ -1,14 +1,10 @@
+/**
+ * 예외를 사전확인으로 바꾸기.
+ * 
+ */
 const values = [];
 function getValueForPeriod(periodNumber) {
-  const value = values[periodNumber];
-  if (!value) {
-    throw new Error('value is undefined');
-  }
-  return value;
+  // 예외가 아니라 예상할 수 있는 에러라면, 사전확인 및 default값을 처리해주는 것으로 우아하게 처리할 수 있다. 
+  return values[periodNumber] ?? 0;
 }
 
-try {
-  getValueForPeriod(-10);
-} catch (error) {
-  console.log('에러 발생!');
-}
