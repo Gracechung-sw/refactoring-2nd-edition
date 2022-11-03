5e5d316e015afa7192242381e6d2f7909d4859dc
Gracechung-sw <hjngy0511@gmail.com>
2022-11-03 21:19:02 +0900

    Refactor: Preserve Whole Object

--- ch11/11-4.js
+++ ch11/11-4.js
@@ -1,8 +1,12 @@
+/**
+ * 객체 통째로 넘기기 (Preserve Whole Object)
+ *  함수에 어느정도의 인자를 전달 할 것인가에 관련된 것. 
+ *  매개변수 범위 규정하기. 
+ *  다른 모듈과의 의존성을 잘 살펴보고 범위를 규정해나가야함.
+ */
 export function temperatureAlerts(room, plan) {
   const alerts = [];
-  const low = room.daysTempRange.low;
-  const high = room.daysTempRange.high;
-  if (!plan.withinRange(low, high)) {
+  if (!plan.withinRange(room.daysTempRange)) {
     alerts.push('room temperature went outside range');
   }
 
@@ -14,9 +18,9 @@ export class HeatingPlan {
     this._temperatureRange = temperatureRange;
   }
 
-  withinRange(bottom, top) {
+  withinRange(range) {
     return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+      range.bottom >= this._temperatureRange.low && range.top <= this._temperatureRange.high
     );
   }
 }
