112ed71ff7ba9d142461c962d462a78ceebf92d9
Gracechung-sw <hjngy0511@gmail.com>
2022-11-10 00:08:31 +0900

    Refactor: Pull up field

--- ch12/12-2.js
+++ ch12/12-2.js
@@ -1,9 +1,12 @@
-class Employee {}
-
-class Salesperson extends Employee {
+/**
+ * 필드 올리기 (Pull up field)
+ * 
+ * 앞서 12.1.js의 메서드 올리기와 이유는 동일하고, 그냥 이건 필드라는 것만 다르다. 
+ */
+class Employee {
   #name;
 }
 
-class Engineer extends Employee {
-  #name;
-}
+class Salesperson extends Employee {}
+
+class Engineer extends Employee {}
